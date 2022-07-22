174c390c0ff3ec782449b3fad6002fac853e295a
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-07-22 15:42:41 -0400

    Initial Commit

---
+++ Array2Challenge/Array2ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Array2ChallengeApp.swift
+//  Array2Challenge
+//
+//  Created by Peter Sekesan on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Array2ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

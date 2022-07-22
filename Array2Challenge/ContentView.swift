1940857e9e81d3f5e633fa94707c2cfbbe1dd255
Peter Sekesan <72456128+Pete-Sekesan@users.noreply.github.com>
2022-07-22 15:54:32 -0400

    initial commit, button layouts and cards struct

--- Array2Challenge/ContentView.swift
+++ Array2Challenge/ContentView.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Text("Hello, world!")
+                .padding()
+            Button {
+            } label: {
+                Text("Button 1")
+                    .padding()
+            }
+            Button {
+            } label: {
+                Text("Button 2")
+                    .padding()
+            }
+        }
+        
+
     }
+}
+
+struct playingCard{
+    var cardsArray = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    var suiteArray = ["spades", "clubs", "hearts", "diamonds"]
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
